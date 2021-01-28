d22a4c77019c40b6f4c5e53ccd7e5dc917bbbb2d
Si Hua <yuikazuga_25@hotmail.co.uk>
2021-01-28 17:53:23 -0500

    renamed file to usePokemons

--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { PokemonInfo } from '../types'
+import { kantoStarterPokemons } from '../data/pokemonData'
 
 function usePokemons() {
   const [pokemons, setPokemons] = useState<PokemonInfo[]>([])
 
   useEffect(() => {
     async function getPokemons() {
+      const urls = kantoStarterPokemons.pokemonNames.map(
+        (pokemonName) => `https://pokeapi.co/api/v2/pokemon/{pokemonName}`
+      )
       const results = await Promise.all([
         axios.get<PokemonInfo>(`https://pokeapi.co/api/v2/pokemon/1`),
       ])
