0120c1d0ff839f28aaad5ed01997320a4e599146
Si Hua <yuikazuga_25@hotmail.co.uk>
2021-01-28 17:27:21 -0500

    updated pokemon list

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React from 'react'
 import PokemonCard from './PokemonCard'
 import usePokemons from '../hooks/pokemons'
 
@@ -6,15 +6,16 @@ const PokemonList = () => {
   const { pokemons } = usePokemons()
 
   return (
-    pokemons.map(pokemon => {
-      return (
+    <>
+      { pokemons.map((pokemon) => (
         <PokemonCard
           id={pokemon.id}
           imageURL={pokemon.sprites.front_default}
           name={pokemon.name}
+          abilities={pokemon.abilities}
         />
-      )
-    })
+      ))}
+    </>
 
   )
 }
