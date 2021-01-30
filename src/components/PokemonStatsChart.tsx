9c82662ca6c525336f0e3274c94f832a6a8f84cc
Si Hua <yuikazuga_25@hotmail.co.uk>
2021-01-29 20:21:32 -0500

    feat: pokemon chart

---
+++ src/components/PokemonStatsChart.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useLayoutEffect } from 'react'
+import Chart from 'chart.js'
+
+interface PokemonStatsChartProps {
+  title: string
+  labels: string[] | string[][]
+  values: number[]
+}
+
+export const CHART_MIN_VALUE = 0
+
+const PokemonStatsChart = ({ title, labels, values }: PokemonStatsChartProps) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useLayoutEffect(() => {
+    const canvasContext = canvasRef?.current?.getContext("2d")
+    let radioChart: Chart
+    if (canvasContext) {
+      radioChart = new Chart(canvasContext, {
+        type: 'radar',
+        data: {
+          labels,
+          datasets: [{
+            label: title,
+            data: values,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scale: {
+            gridLines: {
+              color: '#00b4d8'
+            },
+            angleLines: {
+              display: false
+            },
+            ticks: {
+              suggestedMin: 5,
+              suggestedMax: 15
+            }
+          },
+          responsive: true
+        },
+      })
+    }
+    return () => {
+      if (radioChart) radioChart.destroy()
+    }
+  }, [])
+
+  return (
+    <canvas ref={canvasRef} width="400" height="400" />
+  )
+}
+
+export default PokemonStatsChart
