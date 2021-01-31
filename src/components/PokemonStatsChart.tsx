fbd89436308a72ddb8aeb8ed8a0437c2ca46862a
Si Hua <yuikazuga_25@hotmail.co.uk>
2021-01-30 20:22:04 -0500

    updated colors

--- src/components/PokemonStatsChart.tsx
+++ src/components/PokemonStatsChart.tsx
@@ -23,22 +23,8 @@ const PokemonStatsChart = ({ title, labels, values }: PokemonStatsChartProps) =>
           datasets: [{
             label: title,
             data: values,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(255, 99, 132, 1)',
-              'rgba(255, 99, 132, 1)',
-              'rgba(255, 99, 132, 1)',
-              'rgba(255, 99, 132, 1)',
-              'rgba(255, 99, 132, 1)',
-            ],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1,
             pointBackgroundColor: [
               '#ef476f',
