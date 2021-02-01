6fa32854425827eeb59729d5465e66fa94ddff4d
Si Hua <yuikazuga_25@hotmail.co.uk>
2021-02-01 18:15:26 -0500

    feat: app layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import {
   Switch,
   Route,
 } from "react-router-dom"
-
+import Grid from '@material-ui/core/Grid';
 import PokemonTable from './components/PokemonTable'
+
 
 function App() {
   return (
@@ -16,7 +17,21 @@ function App() {
       <Router>
         <Switch>
           <Route path='/'>
-            <PokemonTable />
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                Header
+              </Grid>
+              <Grid container xs={12} >
+                <Grid xs={1} lg={3} />
+                <Grid item xs={10} lg={6}>
+                  <PokemonTable />
+                </Grid>
+                <Grid xs={1} lg={3} />
+              </Grid>
+              <Grid item xs={12}>
+                Footer
+              </Grid>
+            </Grid>
           </Route>
         </Switch>
       </Router>
